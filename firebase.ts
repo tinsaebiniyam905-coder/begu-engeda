45b4998288c727c21869b3b45e0ce2c6b2316f0f
tinsaebiniyam905-coder <tinsaebiniyam905@gmail.com>
2026-03-22 09:41:32 -0700

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -4,10 +4,10 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBg4CnebgRj2n5DspjuELhB4hYidsWToRY",
-  authDomain: "begu-engda.firebaseapp.com",
-  databaseURL: "https://begu-engda-default-rtdb.firebaseio.com",
-  projectId: "begu-engda",
-  storageBucket: "begu-engda.appspot.com",
+  authDomain: "begu-engeda.firebaseapp.com",
+  databaseURL: "https://begu-engeda-default-rtdb.firebaseio.com",
+  projectId: "begu-engeda",
+  storageBucket: "begu-engeda.appspot.com",
   messagingSenderId: "160228118540",
   appId: "1:160228118540:web:a6d1c43fd7538c0099de29"
 };
