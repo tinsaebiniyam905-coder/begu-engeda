09ddeaeda0c4529933127c494218f1ae385205a1
tinsaebiniyam905-coder <tinsaebiniyam905@gmail.com>
2026-03-22 09:24:39 -0700

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -1,9 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from './firebase-applet-config.json';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Initialize Firebase SDK
+const firebaseConfig = {
+  apiKey: "AIzaSyBg4CnebgRj2n5DspjuELhB4hYidsWToRY",
+  authDomain: "begu-engda.firebaseapp.com",
+  databaseURL: "https://begu-engda-default-rtdb.firebaseio.com",
+  projectId: "begu-engda",
+  storageBucket: "begu-engda.appspot.com",
+  messagingSenderId: "160228118540",
+  appId: "1:160228118540:web:a6d1c43fd7538c0099de29"
+};
+
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
