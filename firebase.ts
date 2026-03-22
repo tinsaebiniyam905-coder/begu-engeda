fb182543c328ba27282c90daa71bb3b02c02cb88
tinsaebiniyam905-coder <tinsaebiniyam905@gmail.com>
2026-03-22 10:50:22 -0700

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -2,21 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBg4CnebgRj2n5DspjuELhB4hYidsWToRY",
-  authDomain: "begu-engeda.firebaseapp.com",
-  databaseURL: "https://begu-engeda-default-rtdb.firebaseio.com",
-  projectId: "begu-engeda",
-  storageBucket: "begu-engeda.appspot.com",
-  messagingSenderId: "160228118540",
-  appId: "1:160228118540:web:a6d1c43fd7538c0099de29"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-
 export enum OperationType {
   CREATE = 'create',
   UPDATE = 'update',
@@ -45,7 +30,22 @@ export interface FirestoreErrorInfo {
   }
 }
 
-export function handleFirestoreError(error: unknown, operationType: OperationType, path: string | null) {
+const firebaseConfig = {
+  apiKey: "AIzaSyBg4CnebgRj2n5DspjuELhB4hYidsWToRY",
+  authDomain: "begu-engeda.firebaseapp.com",
+  databaseURL: "https://begu-engeda-default-rtdb.firebaseio.com",
+  projectId: "begu-engeda",
+  storageBucket: "begu-engeda.appspot.com",
+  messagingSenderId: "160228118540",
+  appId: "1:160228118540:web:a6d1c43fd7538c0099de29"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export const handleFirestoreError = (error: unknown, operationType: OperationType, path: string | null) => {
   const errInfo: FirestoreErrorInfo = {
     error: error instanceof Error ? error.message : String(error),
     authInfo: {
@@ -66,4 +66,4 @@ export function handleFirestoreError(error: unknown, operationType: OperationTyp
   }
   console.error('Firestore Error: ', JSON.stringify(errInfo));
   throw new Error(JSON.stringify(errInfo));
-}
+};
