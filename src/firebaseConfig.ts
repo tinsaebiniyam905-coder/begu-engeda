5d75513e5e7afb67f5720f918e9c146621322b56
tinsaebiniyam905-coder <tinsaebiniyam905@gmail.com>
2026-03-27 01:18:42 -0700

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,17 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, push, onValue, set } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBg4CnebgRj2n5DspjuELhB4hYidsWToRY",
-  authDomain: "begu-engda.firebaseapp.com",
-  databaseURL: "https://begu-engda-default-rtdb.firebaseio.com",
-  projectId: "begu-engda",
-  storageBucket: "begu-engda.firebasestorage.app",
+  apiKey: "AIzaSy...",
+  authDomain: "begu-engeda.firebaseapp.com",
+  databaseURL: "https://begu-engeda-default-rtdb.firebaseio.com",
+  projectId: "begu-engeda",
+  storageBucket: "begu-engeda.appspot.com",
   messagingSenderId: "160228118540",
-  appId: "1:160228118540:web:a6d1c43fd7538c0099de29",
-  measurementId: "G-FDSJBH4FB0"
+  appId: "1:160228118540:web:a6d1c43fd7538c0099de29"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
